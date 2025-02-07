afa0ebaee051d0b0fb4229fd052e8f2b3f724b2e
Sesha Sai Aneesh Teja Vempa <161967671+Atvempa@users.noreply.github.com>
2025-02-06 20:20:51 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,9 @@ from app.config import Config
 
 app = Flask(__name__)
 CORS(app)  # Add CORS support
-
-# Skip auth for API routes
-@app.before_request
-def skip_auth():
-    if request.path.startswith('/api/'):
-        return None
-
 app.config.from_object(Config)
 
 # Initialize MongoDB
 mongo = PyMongo(app)
 
-from app import routes
+from app import routes 
