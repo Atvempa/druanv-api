b6c0043ce0206b3082a490081c96727fc4025c66
Sesha Sai Aneesh Teja Vempa <161967671+Atvempa@users.noreply.github.com>
2025-02-07 00:22:50 -0500

    Delete app directory

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    MONGO_URI = os.getenv('MONGO_URI')
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    OPENAI_API_BASE = os.getenv('OPENAI_API_BASE', 'https://api.openai.com/v1') 
